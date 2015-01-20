a9fe76c035fd45864fb36ff87b4e925b6b5f7ef0
vincent bouillart <vincent.bouillart@ida-mediafoundry.be>
2015-01-20 09:49:54 +0100

    new datepicker

--- app/scripts/directives/tinkDatepicker2.js
+++ app/scripts/directives/tinkDatepicker2.js
@@ -3,28 +3,73 @@ angular.module('tink.datepicker', [])
 .directive('tinkDatepickerNew',['$q','$templateCache','$http','$compile','dateCalculator',function($q,$templateCache,$http,$compile,dateCalculator) {
   return {
     restrict:'EA',
+    require:'ngModel',
     replace:true,
     templateUrl:'templates/tinkDatePicker2.html',
     scope:{
-      ngModel:"&"
+      ngModel:'&'
     },
     link:function(scope,element,attr,ctrl){
       var input = element.find('input');
       var clickable = element.find('span');
 
       scope.$show = function(){
-
+        templateElem.css({position: 'absolute', display: 'block'});
+        element.append(templateElem);
+        var date = dateCal($directive.viewDate);
+        if(date !== null){
+          ctrl.$setViewValue(dateCalculator.formatDate(date, options.dateFormat));
+        }
       };
 
+      var dateCal = function(date){
+        if(angular.isDate(date)){
+          if(date.toString() !== 'Invalid Date'){
+            return date;
+          }else{
+            return null;
+          }
+        }else{
+          if(/^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.test(date)){
+            var dateObject = dateCalculator.getDate(date, options.dateFormat);
+            return dateObject;
+          }else{
+            return null;
+          }
+        }
+      }
+
+      scope.$destroy = function(){
+        templateElem.remove();
+      }
+
+      ctrl.$parsers.unshift(function(viewValue) {console.log(viewValue)
+        input.val(viewValue);
+        return new Date();
+      });
+
+      ctrl.$formatters.push(function(modelValue) {
+        var date = dateCal($directive.viewDate);
+        if(date !== null){
+          ctrl.$setViewValue(dateCalculator.formatDate(date, options.dateFormat));
+        }
+        return modelValue;
+      });
+
+      clickable.bind('click touch',function(){
+        scope.$show();
+      })
+
       var options = {
-        yearTitleFormat:'yyyy'
+        yearTitleFormat:'yyyy',
+        dateFormat:'dd/mm/yyyy'
       }
 
       var $directive = {
         viewDate: new Date()
       }
 
-      scope.$build= function() {
+      scope.$build = function() {
         scope.title = dateCalculator.format($directive.viewDate, options.yearTitleFormat);
         scope.rows =  dateCalculator.daysInRows($directive.viewDate);
       }
@@ -58,9 +103,6 @@ angular.module('tink.datepicker', [])
         // --- store the html we retrieved //
         templateElem = $compile(template);
         templateElem = templateElem(scope, function () {});
-        templateElem.css({position: 'absolute', display: 'block'});
-        element.append(templateElem);
-        scope.$build();
       });
 
     }
