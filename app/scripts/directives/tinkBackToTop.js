3808513957170c650342c0c807eb63f7462c4cec
Lieven VG <lievenvg@gmail.com>
2015-03-16 21:22:16 +0100

    Merged development. Removed html design example from TomH. Added JQuery for screenreader and moved class .back-to-top from html to script.

--- app/scripts/directives/tinkBackToTop.js
+++ app/scripts/directives/tinkBackToTop.js
@@ -8,26 +8,28 @@ angular.module('tink.backtotop', [])
 
       jQuery(document).ready(function($){ //browser window scroll (in pixels) after which the "back to top" link is shown
         var offset = 300, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-          offset_opacity = 1200,
-          scroll_top_duration = 200, //duration of the top scrolling animation (in ms)
-          $back_to_top = element; //grab the "back to top" link
+          offsetOpacity = 1200,
+          scrollTopDuration = 200, //duration of the top scrolling animation (in ms)
+          $backToTop = element; //grab the "back to top" link
+          $backToTop.addClass('back-to-top'); //adding the cladd back-to-top so it's not needed in the html
+          $backToTop.append( '<span>Terug naar boven</span>');
 
         $(window).scroll(function(){ //hide or show the "back to top" link
-          ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('is-visible') : $back_to_top.removeClass('is-visible btn-fade-out');
+          ( $(this).scrollTop() > offset ) ? $backToTop.addClass('is-visible') : $backToTop.removeClass('is-visible btn-fade-out');
           // disabled this function because we're going to use an animation instead
-          // if( $(this).scrollTop() > offset_opacity ) {
+          // if( $(this).scrollTop() > offsetOpacity ) {
           //   $back_to_top.addClass('btn-fade-out');
           // }
         });
 
-        $back_to_top.on('click', function(event){ //smooth scroll to top
+        $backToTop.on('click', function(event){ //smooth scroll to top
           event.preventDefault();
           $('body,html').animate({
             scrollTop: 0 , //the top = 0
-            }, scroll_top_duration
+            }, scrollTopDuration
           );
         });
-      })
+      });
       }
-    }
+    };
   }]);
